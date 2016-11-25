3ee723f4502ce967b95594a65afa6aaefad67029
monica10125 <monica.cas195@hotmail.com>
2016-11-25 06:46:52 -0500

    descarga en pdf del detalle de produtos

--- resources/views/template/admin/productos/pdf.blade.php
+++ resources/views/template/admin/productos/pdf.blade.php
@@ -1,1 +1,47 @@
-<h1>reporte pdf</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+    <title>Pdf</title>
+</head>
+
+<body>
+<h1 class="text-center text-info">Lista de productos franquicia Coffenet</h1>
+
+
+
+<div class="col-sm-12">
+
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>estado</th>
+            <th>Precio</th>
+            <th>Stock</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach($productos as $producto)
+        <tr>
+            <td>{{$producto->nombre}}</td>
+            <td>{{$producto->descripsion}}</td>
+            <td>{{$producto->estado}}</td>
+            <td>{{$producto->valor}}</td>
+            <td>{{$producto->cantidades}}</td>
+
+        </tr>
+@endforeach
+        </tbody>
+    </table>
+
+
+</div>
+
+</body>
+</html>
